10c7b35c8e791962dbe48e0a847fd0c18ea627c3
AlexqPr <isip_a.v.mic@mpt.ru>
2023-04-22 20:57:38 +0300

    Добавьте файлы проекта.

---
+++ Caleendar5/Punkt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caleendar5
+{
+    internal class Punkt
+    {
+        public string Name;
+        public string Put;
+        public bool vibran;
+    }
+}
